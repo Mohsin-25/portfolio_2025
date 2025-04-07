c2721a271889e8d93106f256f775a8dab6c2422d
Mohsin-25 <mohsinmohammad122@gmail.com>
2025-04-08 02:20:34 +0530

    changed skills section

--- src/Components/Info/Info.jsx
+++ src/Components/Info/Info.jsx
@@ -8,7 +8,7 @@ const Info = () => {
       <Navbar />
       <section className="page info flex-col" id="info">
         {/* <div className="flex"> */}
-        <div className="flex flex-col gap-8 justify-center text-center h-full">
+        <div className="flex flex-col gap-8 justify-center text-center h-full my-auto">
           <div className="flex flex-col">
             <RevealAnimation>
               <h1 className="!mb-0">Building the Web,</h1>
